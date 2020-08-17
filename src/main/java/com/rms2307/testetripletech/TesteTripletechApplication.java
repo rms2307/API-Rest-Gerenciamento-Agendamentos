16dd546e3b0990387eab125390c4f8b3aeba3c1c
Robson Silva <rms2307@outlook.com>
2020-08-17 18:07:59 -0300

    primeiro commit

---
+++ src/main/java/com/rms2307/testetripletech/TesteTripletechApplication.java
@@ -0,0 +1,13 @@
+package com.rms2307.testetripletech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTripletechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTripletechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rms2307/testetripletech/TesteTripletechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rms2307.testetripletech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTripletechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
