dc40e43a095ebb34dce3055d2c9a15985f69a582
Robson Silva <rms2307@outlook.com>
2020-08-18 22:59:12 -0300

    atualizando um agendamento

--- src/main/java/com/rms2307/testetripletech/resources/AgendamentoResource.java
+++ src/main/java/com/rms2307/testetripletech/resources/AgendamentoResource.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rms2307.testetripletech.domain.Agendamento;
 import com.rms2307.testetripletech.dto.AgendamentoNewDTO;
+import com.rms2307.testetripletech.dto.AgendamentoUpdateDTO;
 import com.rms2307.testetripletech.services.AgendamentoService;
 
 @RestController
@@ -36,6 +39,14 @@ public class AgendamentoResource {
 		service.salvarPessoaAgendamento(objDTO, obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> atualizar(@RequestBody AgendamentoUpdateDTO objDTO, @PathVariable Integer id) {
+		Agendamento obj = service.converterObjDTOparaObj(objDTO);
+		obj.setId(id);
+		obj = service.atualizar(obj);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/rms2307/testetripletech/services/AgendamentoService.java
+++ src/main/java/com/rms2307/testetripletech/services/AgendamentoService.java
@@ -10,6 +10,7 @@ import com.rms2307.testetripletech.domain.Agendamento;
 import com.rms2307.testetripletech.domain.Pessoa;
 import com.rms2307.testetripletech.domain.PessoaAgendamento;
 import com.rms2307.testetripletech.dto.AgendamentoNewDTO;
+import com.rms2307.testetripletech.dto.AgendamentoUpdateDTO;
 import com.rms2307.testetripletech.repositories.AgendamentoRepository;
 import com.rms2307.testetripletech.repositories.PessoaAgendamentoRepository;
 import com.rms2307.testetripletech.repositories.PessoaRepository;
@@ -35,9 +36,24 @@ public class AgendamentoService {
 		return repo.save(obj);
 	}
 
+	public Agendamento atualizar(Agendamento obj) {
+		Optional<Agendamento> novoObj = repo.findById(obj.getId());
+		atualizarDados(novoObj.orElseThrow(null), obj);
+		return repo.save(novoObj.orElseThrow(null));
+	}
+
+	public void atualizarDados(Agendamento novoObj, Agendamento obj) {
+		novoObj.setDataInicio(obj.getDataInicio());
+		novoObj.setDataFim(obj.getDataFim());
+	}
+
 	public Agendamento converterObjDTOparaObj(AgendamentoNewDTO objDTO) {
 		Agendamento obj = new Agendamento(null, null, objDTO.getDataInicio(), objDTO.getDataFim());
 		return obj;
+	}
+
+	public Agendamento converterObjDTOparaObj(AgendamentoUpdateDTO objDTO) {
+		return new Agendamento(objDTO.getId(), null, objDTO.getDataInicio(), objDTO.getDataFim());
 	}
 
 	public void salvarPessoaAgendamento(AgendamentoNewDTO objDTO, Agendamento obj) {
