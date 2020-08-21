717f339b191d21d8eea4b1f5cb1cc347dae0e866
Robson Silva <rms2307@outlook.com>
2020-08-21 11:52:23 -0300

    resolvendo problema de CrossOrigin

--- src/main/java/com/rms2307/testetripletech/resources/AgendamentoResource.java
+++ src/main/java/com/rms2307/testetripletech/resources/AgendamentoResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import com.rms2307.testetripletech.services.CSVService;
 
 @RestController
 @RequestMapping(value = "/agendamentos")
+@CrossOrigin("*")
 public class AgendamentoResource {
 
 	@Autowired
